c4ba936ee2779ded6b498300a1db65df17fd0d8d
msimo9 <matjaz.simonic@os-ajdovscina.si>
2022-08-19 07:24:36 +0200

    cm5

--- scripts/login.js
+++ scripts/login.js
@@ -1,3 +1,44 @@
+import {app, auth, db} from './firebase.js';
+import { createUserWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-auth.js";
+import { doc, setDoc } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-firestore.js";
+
+const checkIfLoggedIn = () =>{
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+            const uid = user.uid;
+            console.log("logged in");
+        } else {
+            console.log("not logged in");
+            renderFields();
+        }
+    });
+}
+
+const addUserData = async(userID, userData) => {
+    await setDoc(doc(db, "userInfo", userID), {
+        userID: userID,
+        email: userData.email,
+        fullName: userData.fullName,
+        code: userData.code,
+        lessonsDone: [],
+        timeSpentTyping: 0,
+    });
+}
+
+const createAccount = (userData) =>{
+    createUserWithEmailAndPassword(auth, userData.email, userData.password)
+    .then((userCredential) => {
+        const user = userCredential.user;
+        addUserData(user.uid, userData);
+        console.log("sign up successful!")
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, ": ", errorMessage);
+    });
+}
+
 const formContainer = document.createElement("div");
 formContainer.classList.add("login-form-container");
 
@@ -26,16 +67,38 @@ const renderFields = () =>{
     Object.keys(userInfo).forEach((item, index) => {
         if(operation === "login" && index < 2 || operation === "signup"){
             const inputFieldTitle = document.createElement("h3");
+
+            function appendInfoCircle(){
+                const infoDiv = document.createElement("div");
+                infoDiv.setAttribute("id", "code-information-div");
+                infoDiv.innerText = "Kodo ti priskrbi učitelijca ali učitelj. Brez pravilne in ustrezne kode registracija računa ni mogoča.";
+                const infoIcon = document.createElement("ion-icon");
+                infoIcon.setAttribute("name", "information-circle-outline");
+                infoIcon.setAttribute("id", "code-information-icon");
+                infoIcon.addEventListener("mouseenter", ()=>{
+                    infoDiv.style.display = "flex";
+                });
+                infoIcon.addEventListener("mouseleave", ()=>{
+                    infoDiv.style.display = "none";
+                });
+                inputFieldTitle.appendChild(infoIcon);
+                inputFieldTitle.appendChild(infoDiv);
+            }
+
             switch(index){
                 case 0: inputFieldTitle.innerText = "Elektronski naslov"; break;
                 case 1: inputFieldTitle.innerText = "Geslo"; break;
                 case 2: inputFieldTitle.innerText = "Ponovi geslo"; break;
                 case 3: inputFieldTitle.innerText = "Ime in priimek"; break;
-                case 4: inputFieldTitle.innerText = "Koda"; break;
+                case 4: inputFieldTitle.innerHTML = "Koda"; appendInfoCircle(); break;
                 default: break;
             }
+
             inputFieldsContainer.appendChild(inputFieldTitle);
             const inputField = document.createElement("input");
+            inputField.addEventListener("keyup", ()=>{
+                userInfo[item] = inputField.value; 
+            });
             if(index === 1 || index === 2){
                 const passwordWrapper = document.createElement("div");
                 inputField.setAttribute("type", "password");
@@ -67,6 +130,20 @@ const renderFields = () =>{
     const submitButton = document.createElement("div");
     submitButton.classList.add("form-submit-button");
     submitButton.innerText = operation === "login" ? "Prijava" : "Registracija";
+    const warningMessage = document.createElement("div");
+    warningMessage.setAttribute("id", "incorrect-info-warning");
+    submitButton.appendChild(warningMessage);
+    submitButton.addEventListener("click", ()=>{
+        if(operation === "signup" && userInfo.password === userInfo.passwordRepeat && userInfo.email.includes("@os-ajdovscina.si") && userInfo.fullName.length !== 0 && userInfo.code.length !== 0){
+            operation === "login" ? null : createAccount(userInfo);
+        }else{
+            warningMessage.innerText = "Prosim, izpolni vsa polja pravilno.";
+            setTimeout(()=>{
+                warningMessage.innerText = "";
+            }, 5000)
+        }
+        
+    });
     formContainer.appendChild(submitButton);
 
 
@@ -90,4 +167,5 @@ const renderFields = () =>{
     document.body.appendChild(formContainer);
 }
 
-renderFields();
+//renderFields();
+checkIfLoggedIn();
